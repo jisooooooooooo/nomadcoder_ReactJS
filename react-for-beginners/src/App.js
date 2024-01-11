4066f4b67aeeef0d13d554000ee2ae3eec6875fa
jisooooooooooo <dlawltn0708@naver.com>
2024-01-11 17:47:52 +0900

    todo list part2

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -9,11 +9,9 @@ function App() {
     if (toDo === "") {
       return;
     }
-
     setToDos((currentArray) => [toDo, ...currentArray]);
     setToDo("");
   };
-
   return (
     <div>
       <h1>My To Dos ({toDos.length})</h1>
@@ -26,6 +24,12 @@ function App() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
