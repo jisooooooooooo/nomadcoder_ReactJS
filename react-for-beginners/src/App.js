aadf7c5a23b5500b092c7749c149535733a942b6
jisooooooooooo <dlawltn0708@naver.com>
2024-01-11 20:44:35 +0900

    react router

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,6 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 export const App = () => {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/hello">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/movie" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
